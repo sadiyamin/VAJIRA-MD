9813cfdd176c7f86ddf72717b9eb04155adb3e61
ᴍ.ʀ sͥᴀͣᴅͫᴇᴇsʜᴀ <97608407+sadiyamin@users.noreply.github.com>
2024-08-04 12:28:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> SADEESHA - ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94768211853' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=rM0QwZ6K#BUWCQDTHk4knAS5ALdx909iUUK2PBksBLoPOBCX1ZJA' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=uRFlVDYR#L8FEYK9-e1qbJHPTmo0nBvktnGw8oLxx6Rc9Golr_Jw' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
